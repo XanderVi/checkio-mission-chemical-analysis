65741e79793350f5382fe4136be1d12f4f9f85f1
Valentin Bryukhanov <bvv.mag@gmail.com>
2013-11-28 16:10:04 +1200

    referee and tests

--- verification/tests.py
+++ verification/tests.py
@@ -7,30 +7,66 @@ Each test is dict with
     "explanation" -- not necessary key, it's using for additional info in animation.
 """
 
-
 TESTS = {
     "Basics": [
         {
-            "input": [3, 2],
-            "answer": 5,
-            "explanation": "3+2=?"
+            "input": 64,
+            "answer": [15, 21, 28],
         },
         {
-            "input": [5, 7],
-            "answer": 12,
-            "explanation": "5+7=?"
-        }
+            "input": 371,
+            "answer": [36, 45, 55, 66, 78, 91],
+        },
+        {
+            "input": 225,
+            "answer": [105, 120],
+        },
+        {
+            "input": 882,
+            "answer": [],
+        },
     ],
     "Extra": [
         {
-            "input": [6, 3],
-            "answer": 9,
-            "explanation": "6+3=?"
+            "input": 631,
+            "answer": [190, 210, 231]
         },
         {
-            "input": [6, 7],
-            "answer": 13,
-            "explanation": "6+7=?"
+            "input": 130,
+            "answer": [21, 28, 36, 45]
+        },
+        {
+            "input": 216,
+            "answer": [6, 10, 15, 21, 28, 36, 45, 55]
+        },
+        {
+            "input": 219,
+            "answer": [3, 6, 10, 15, 21, 28, 36, 45, 55]
+        },
+
+        {
+            "input": 994,
+            "answer": []
+        },
+        {
+            "input": 454,
+            "answer": [3, 6, 10, 15, 21, 28, 36, 45, 55, 66, 78, 91]
+        },
+        {
+            "input": 136,
+            "answer": [36, 45, 55]
+        },
+
+        {
+            "input": 5,
+            "answer": []
+        },
+
+        {
+            "input": 10,
+            "answer": [1, 3, 6]
         }
+
+
     ]
 }
